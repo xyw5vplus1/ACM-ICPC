b5437a4bfa650d69de90c823d3ac8196f1cf3ff3
xyw5vplus1 <53569770+xyw5vplus1@users.noreply.github.com>
2020-04-15 12:51:38 +0800

    Update D.cpp

--- Codeforces/Educational/81/D.cpp
+++ Codeforces/Educational/81/D.cpp
@@ -1,4 +1,4 @@
-/*#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 const int maxn=1e5;
 typedef long long ll;
@@ -46,6 +46,6 @@ int main()
         printf("%I64d\n",getPhi(m));
     }
     return 0;
-}*/
+}
 
 
