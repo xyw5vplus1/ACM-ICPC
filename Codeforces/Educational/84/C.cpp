62594a65648670e0822b5cb3b2a511d3bc0042de
xyw5vplus1 <53569770+xyw5vplus1@users.noreply.github.com>
2020-04-15 12:45:40 +0800

    Update C.cpp

--- Codeforces/Educational/84/C.cpp
+++ Codeforces/Educational/84/C.cpp
@@ -1,4 +1,4 @@
-/*#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef double db;
@@ -34,4 +34,4 @@ int main()
         }
     }
     return 0;
-}*/
+}
