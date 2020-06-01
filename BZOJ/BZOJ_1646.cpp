40d0c1803171b812cea5ae9180d6d20daa8c7568
xyw5vplus1 <53569770+xyw5vplus1@users.noreply.github.com>
2020-06-02 00:06:50 +0800

    Add files via upload

---
+++ BZOJ/BZOJ_1646.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for (int i=a;i<b;i++)
+#define per(i,a,b) for (int i=a;i>b;i--)
+#define lson(p) p<<1
+#define rson(p) p<<1|1
+using namespace std;
+typedef long long ll;
+typedef double db;
+const int maxn=1e6+10;
+queue<int> q;
+bool vis[maxn];
+int n,k;
+int dist[maxn];
+int main()
+{
+    scanf("%d%d",&n,&k);
+    q.push(n); vis[n]=true; dist[n]=0;
+    while (q.size()) {
+        int u=q.front(); q.pop();
+        if (u==k) break;
+        if (2*u<maxn&&!vis[2*u]) {
+            vis[2*u]=true; dist[2*u]=dist[u]+1; q.push(2*u);
+        }
+        if (u+1<maxn&&!vis[u+1]) {
+            vis[u+1]=true; dist[u+1]=dist[u]+1; q.push(u+1);
+        }
+        if (u-1>=0&&!vis[u-1]) {
+            vis[u-1]=true; dist[u-1]=dist[u]+1; q.push(u-1);
+        }
+    }
+    printf("%d\n",dist[k]);
+    return 0;
+}
---
+++ BZOJ/BZOJ_4500.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for (int i=a;i<b;i++)
+#define per(i,a,b) for (int i=a;i>b;i--)
+#define lson(p) p<<1
+#define rson(p) p<<1|1
+using namespace std;
+typedef long long ll;
+typedef double db;
+const int maxn=2000+10;
+const int maxm=5000+10;
+const int INF=0x3f3f3f3f;
+struct edge
+{
+    int to,_next,val;
+}G[maxm];
+int head[maxn],num=0;
+bool vis[maxn];
+int in[maxn],dist[maxn];
+int T,n,m,k,x,y,w;
+queue<int> q;
+void add(int u,int v,int w)
+{
+    G[++num].to=v;
+    G[num]._next=head[u];
+    G[num].val=w;
+    head[u]=num;
+}
+void dfs(int u)
+{
+    vis[u]=true;
+    for (int i=head[u];i;i=G[i]._next) {
+        int v=G[i].to;
+        if (!vis[v]) dfs(v);
+    }
+}
+bool SPFA(int root,int cnt)
+{
+    memset(vis,false,sizeof(vis));
+    memset(in,0,sizeof(in));
+    rep(i,0,cnt+1) dist[i]=INF;
+    dist[root]=0; in[root]=1; q.push(root);
+    while (q.size()) {
+        int u=q.front(); q.pop(); vis[u]=false;
+        for (int i=head[u];i;i=G[i]._next) {
+            int v=G[i].to;
+            if (dist[v]>dist[u]+G[i].val) {
+                dist[v]=dist[u]+G[i].val;
+                if (!vis[v]) {
+                    vis[v]=true; in[v]++; q.push(v);
+                    if (in[v]>cnt) return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+int main()
+{
+    scanf("%d",&T);
+    while (T--) {
+        memset(head,0,sizeof(head)); num=0;
+        scanf("%d%d%d",&n,&m,&k);
+        rep(i,0,k) {
+            scanf("%d%d%d",&x,&y,&w);
+            add(x,n+y,-w); add(n+y,x,w);
+        }
+        memset(vis,false,sizeof(vis));
+        rep(i,1,n+1)
+        if (!vis[i]) {
+            dfs(i); add(0,i,0);
+        }
+        if (SPFA(0,n+m+1)) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
